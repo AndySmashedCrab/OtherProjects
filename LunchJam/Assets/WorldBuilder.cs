e183b8e76f77809382e06132f9d5a22413c20201
AndySmashedCrab <30597137+AndySmashedCrab@users.noreply.github.com>
2018-10-19 16:51:40 +0100

    First Commit

---
+++ LunchJam/Assets/WorldBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBuilder : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] tile;
+    List<GameObject> world;
+    void Start()
+    {        
+        world = new List<GameObject>();
+        for (int i = 0; i < 20; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                world.Add(Instantiate(tile[Random.Range(0,tile.Length)], new Vector3(i-10, (Random.value - 0.5f)*0.1f, j-10), Quaternion.identity).gameObject);
+            }
+        }
+    }
+}
